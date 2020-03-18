62417f1b73e5bb1234ab8f008cf5b55e2caec291
Rubén Zamora <ruben.zamora@leanmind.es>
2020-03-18 16:45:37 +0000

    Configuracion de la conexion a postgres

--- src/main/java/com/platzi/ereservation/modelo/Cliente.java
+++ src/main/java/com/platzi/ereservation/modelo/Cliente.java
@@ -1,15 +1,32 @@
 package com.platzi.ereservation.modelo;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Set;
 
 /**
  * Clase que representa la tabla Cliente
  */
 
 @Data
+@Entity
+@Table(name = "cliente")
 public class Cliente {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String idCli;
     private String nombreCli;
     private String apellidoCli;
+    private String identificacionCli;
+    private String direccionCli;
+    private String telefonoCli;
+    private String emailCli;
+
+    @OneToMany(mappedBy = "cliente")
+    private Set<Reserva> reservas;
 
     public Cliente(){
 
---
+++ src/main/java/com/platzi/ereservation/modelo/Reserva.java
@@ -0,0 +1,30 @@
+package com.platzi.ereservation.modelo;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.ManyToAny;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Tabla reserva
+ */
+
+@Data
+@Entity
+@Table(name = "reserva")
+public class Reserva {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String idRes;
+    private Date fechaIngresoRes;
+    private Date fechaSalidaRes;
+    private int cantidadPersonasRes;
+    private String descripcionRes;
+
+    @ManyToOne
+    @JoinColumn(name = "idCli")
+    private Cliente cliente;
+}
