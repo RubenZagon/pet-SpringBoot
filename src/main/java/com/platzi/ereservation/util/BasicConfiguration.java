7eb7e4be2f108445b078f4ec19b2a45660825120
Rubén Zamora <ruben.zamora@leanmind.es>
2020-03-19 16:32:53 +0000

    Creación de login
    Creación simple de login, siguiente paso personalizar el login

---
+++ src/main/java/com/platzi/ereservation/util/BasicConfiguration.java
@@ -0,0 +1,24 @@
+package com.platzi.ereservation.util;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Para manejar el tema de seguridad
+ */
+
+@Configuration
+@EnableWebSecurity
+public class BasicConfiguration extends WebSecurityConfigurerAdapter {
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+}
